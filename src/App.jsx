df91b56134f8745b30d1105c41e52aa0e1f8998b
NABAJYOTI <117579785+Neyvil@users.noreply.github.com>
2024-02-04 19:46:20 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1
+    wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      PopularProducts
+    </section>
+    <section className="padding">
+      SuperQuality
+    </section>
+    <section className="padding">
+      Services
+    </section>
+    <section className="padding">
+      SpecialOffers
+    </section>
+  </main>
+);
